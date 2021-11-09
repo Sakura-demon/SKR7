e19a8a88a72ee823e229b89cbe237765268228e0
raven <2638185799@qq.com>
2021-11-09 23:40:47 +0800

    修改密码Password_Change类的更新，可能还有NullPointerException异常

--- src/main/java/com/example/SKR7/Password_Change.java
+++ src/main/java/com/example/SKR7/Password_Change.java
@@ -3,11 +3,14 @@
  */
 package com.example.SKR7;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
 
 public class Password_Change extends HttpServlet {
     @Override
@@ -20,5 +23,81 @@ public class Password_Change extends HttpServlet {
         如果一致调用Password_Change存储过程
         返回调用返回结果给前端
          */
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/SKR7?serverTimezone=GMT&characterEncoding=utf-8","root","12345678");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        CallableStatement sql = null;
+
+        JSONArray jsonArray = new JSONArray();
+        HttpSession session = req.getSession();
+        String userId = (String) session.getAttribute("Uid");
+        String old_psw = (String) session.getAttribute("old_psw");
+        String new_psw = (String) session.getAttribute("new_psw");
+        String re_psw = (String) session.getAttribute("re_psw");
+        if (new_psw.equals(re_psw)) {
+            try {
+                sql = conn.prepareCall("{call Password_Change(?, ?, ?)}");
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+            ResultSet rs = null;
+            JSONObject jsonObjectPswChange = new JSONObject();
+            try {
+                sql.setString(1, userId);
+                sql.setString(2, old_psw);
+                sql.setString(3,new_psw);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                rs = sql.executeQuery();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+            try {
+                if (rs.getString("result").equals("1")) {
+                    jsonObjectPswChange.put("result", 1);
+                } else {
+                    jsonObjectPswChange.put("result", 0);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                rs.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+            try {
+                sql.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+            try {
+                conn.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        } else {
+            JSONObject jsonObjectPswChange = new JSONObject();
+            jsonObjectPswChange.put("result", -1);
+            jsonArray.add(jsonObjectPswChange);
+        }
+
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter writer = resp.getWriter();
+        writer.println(jsonArray);
+        writer.flush();
+        writer.close();
     }
 }
