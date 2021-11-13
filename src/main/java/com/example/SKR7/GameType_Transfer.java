662f360f2d94117f432444e22d8261807483372e
xiamilei <93518248+xiamilei@users.noreply.github.com>
2021-11-13 16:53:13 +0800

    Update GameType_Transfer.java

--- src/main/java/com/example/SKR7/GameType_Transfer.java
+++ src/main/java/com/example/SKR7/GameType_Transfer.java
@@ -12,6 +12,10 @@ import java.io.IOException;
 public class GameType_Transfer extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String type = req.getParameter("Gname");
+        HttpSession session = req.getSession();
+        session.setAttribute("Gid",name);
+        session.setAttribute("Game","TYPE");
         /*
         接收前端发来游戏名
         将其保存到cookie内
