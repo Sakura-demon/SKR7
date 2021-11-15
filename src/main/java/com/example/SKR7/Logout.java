925b4883c4819fa963b143d00415a52ce40b3bc0
01 <2234181842@qq.com>
2021-11-15 21:18:46 +0800

    修改代码

--- src/main/java/com/example/SKR7/Logout.java
+++ src/main/java/com/example/SKR7/Logout.java
@@ -25,6 +25,7 @@ public class Logout extends HttpServlet {
         Cookie cookie = new Cookie("Login", "1");
         //设置cookie有效时间
         cookie.setMaxAge(24*60*60);
+
         //添加cookie描述
         cookie.setComment("Login");
         //设置cookie有效路径为全局
